ffa9060b0d0952c7f46b586ea4c300e1445ba101
vitaliidox <72409488+vitaliidox@users.noreply.github.com>
2021-05-07 13:03:37 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+//Task 1
+
+// let obj = {param1 : "333",
+//     param2: "jff"}
+
+// let arrParram = []
+// let arrVal = []
+
+// function json(obj){
+//     for(let key in obj){
+//         arrParram.push(key)
+//         arrVal.push(obj[key])
+//     }
+//     let objJson = {[arrParram]:arrVal}
+
+// console.log(JSON.stringify(objJson))
+// }
+// json(obj)
+
+//Task 2
+
+// let user = {
+//     name: 'Vasya',
+//     surname: 'Mishak',
+//     age: 20,
+//     isMale: true,
+//     params: {
+//         height: 175,
+//         width: 160,
+//         weight: 220
+//     }
+// };
+
+// function flatMap(obj){
+
+// for(let k in obj.params){
+//     obj[`${k}`] = obj.params[k]
+// }
+
+// delete obj.params
+// console.log(JSON.stringify(obj))
+
+// }
+// flatMap(user)
+
